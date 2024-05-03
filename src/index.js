305523dcaa5287717ae1f95fa76e8235bec97bb0
David Brandon <davidpbrandon@gmail.com>
2024-05-03 16:34:14 +0100

    Restoring original theme

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import "../css/style.scss"; // scss for gulp
 import AOS from "aos"; // animate on scroll package
 import "aos/dist/aos.css"; // AOS CSS
 //import Masonry from "masonry-layout";
-//import Typed from "typed.js";
+import Typed from "typed.js";
 import searchAPI from "./modules/searchAPI"; // Duh...
 import StorageCookiePopup from "./modules/storageCookie";
 import MenuOverlayShow from "./modules/menuOverlay";
 import StickyEls from "./modules/stickyEls";
 import FormHandler from "./modules/formHandler";
-//import { initializeTyped, initializeTypedBlog } from "./modules/typed";
+import { initializeTyped, initializeTypedBlog } from "./modules/typed";
 
-//const menuOverlayShow = new MenuOverlayShow();
+const menuOverlayShow = new MenuOverlayShow();
 const storageCookie = new StorageCookiePopup();
-//const stickyEls = new StickyEls();
+const stickyEls = new StickyEls();
 const formHandler = new FormHandler();
 
 AOS.init();
@@ -27,11 +27,9 @@ searchAPI();
   }
 });*/
 
-/*
 document.addEventListener("DOMContentLoaded", function () {
   initializeTyped();
 });
 document.addEventListener("DOMContentLoaded", function () {
   initializeTypedBlog();
 });
-*/
